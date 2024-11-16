ecb377d33cc487ba9b3fb151dc78562402008270
ryanebch <ryanesgit@gmail.com>
2024-11-16 15:14:17 +0100

    clientss

--- index.php
+++ index.php
@@ -20,25 +20,17 @@
 
 <body>
 <nav class="navbar_f navbar-expand-lg navbar-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Nouvelle Barre</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="#">Accueil</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contact</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-  
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Nouvelle Barre</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+        <a class="nav-link" href="#">CabinetPlus</a>
+        </ul>
 </nav>
-<nav class="navbar navbar-expand-lg navbar-dark fs-6 mb-5 fixed-top" style="background-color: #72A0C1">
+<nav class="navbar navbar-expand-lg navbar-dark fs-2 mb-5 fixed-top" style="background-color: #72A0C1">
   <div class="container-fluid">
     <div class="collapse navbar-collapse d-flex justify-content-center" id="navbarNav">
       <ul class="navbar-nav">
@@ -69,8 +61,8 @@
   ?>
    <a href="add_new.php" class= "btn btn-dark mb-3" style ="background-color:#545AA7;border-color= #545AA7">Ajouter un nouvel utilisateur</a>
    <form action="" method="get" class="mb-3">
-  <input type="text" name="search" class="form-control mb-3" placeholder="Rechercher un utilisateur" />
-</form>      
+   <input type="text" name="search" class="form-control mb-3" placeholder="Rechercher un utilisateur" />
+   </form>      
 <table class="table table-hover text-center">
       <thead class="table-dark">
         <tr>
